1bd23b8146ab17736b50dbe9865fa4f60835de01
Geraldo Fada <geraldofada.neto@gmail.com>
2020-07-21 18:37:03 -0300

    moody and minons installed for new mode-line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,6 +52,7 @@
 (require 'use-package)
 
 ;; basic packages init
+;; must have
 (use-package counsel :ensure t
     :config
     (ivy-mode 1)
@@ -59,18 +60,33 @@
 (use-package counsel-projectile :ensure t)
 (use-package evil :ensure t
     :config (evil-mode 1))
+(use-package magit :ensure t)
+(use-package evil-magit :ensure t)
+;; fluffy
+(use-package zenburn-theme :ensure t
+    :config
+    (load-theme 'zenburn t)
+    (let ((line (face-attribute 'mode-line :underline)))
+    (set-face-attribute 'mode-line          nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :underline  line)
+    (set-face-attribute 'mode-line          nil :box        nil)
+    (set-face-attribute 'mode-line-inactive nil :box        nil)
+    (set-face-attribute 'mode-line-inactive nil :background "gray22")))
+(use-package minions :ensure t
+    :config (minions-mode 1))
+(use-package moody :ensure t
+    :config
+    (setq x-underline-at-descent-line t)
+    (moody-replace-mode-line-buffer-identification)
+    (moody-replace-vc-mode))
+;; utility packages
 (use-package which-key :ensure t
     :config (which-key-mode))
 (use-package projectile :ensure t
     :config
     (projectile-mode +1)
     (setq projectile-completion-system 'ivy))
-(use-package magit :ensure t)
-(use-package evil-magit :ensure t)
-(use-package zenburn-theme :ensure t
-    :config (load-theme 'zenburn t))
-
-;; utility packages
 (use-package avy :ensure t
     :config (setq avy-timeout-seconds 0.3))
 
@@ -120,7 +136,6 @@
     "pb"  '(projectile-switch-to-buffer :wk "project buffer")
     "pk"  '(projectile-kill-buffers :wk "project kill buffers")
     "pp"  '(projectile-command-map :wk "projectile command")
-
     ;; quit
     "q"   '(:ignore t :wk "quit")
     "qq"  '(save-buffers-kill-emacs :wk "save and quit")
