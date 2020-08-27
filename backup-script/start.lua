1b0ffa6146f81e07ce4060a3db6f6022536b2983
Geraldo Fada <geraldofada.neto@gmail.com>
2020-08-27 18:43:14 -0300

    new functions to check size limit, get oldest file and new json parser dep

--- backup-script/start.lua
+++ backup-script/start.lua
@@ -4,6 +4,7 @@
 -- =============
 -- CONFIG
 --
+SIZE_LIMIT = 10737418240 -- 10GB
 to_backup = {
   ["slipbox"] = {
     path_local = "z:/notes/slipbox",
@@ -20,6 +21,41 @@ to_backup = {
 -- =============
 -- CODE
 --
+
+json = require "json"
+
+prog_handle = nil
+prog_str_handle = nil
+
+-- This function returns true if the
+-- cloud_path reached the max_bytes size
+-- limit.
+function is_on_size_limit(max_bytes, cloud_path)
+  prog_handle = io.popen("rclone size --json " .. cloud_path)
+  prog_str_handle = prog_handle:read()
+  local json_value = json.decode(prog_str_handle)
+
+  if json_value["bytes"] >= max_bytes then
+    return true
+  else
+    return false
+  end
+
+  prog_handle:close()
+end
+
+-- This function returns the first file
+-- in cloud_path (alphabetical order).
+-- Using a timestamp as a filename the oldest
+-- is the first in this case.
+function return_oldest_file(cloud_path)
+  prog_handle = io.popen("rclone lsf " .. cloud_path)
+  prog_str_handle = prog_handle:read()
+  prog_handle:close()
+
+  return prog_str_handle
+end
+
 function check_tools()
   assert(os.execute("rclone --version"))
   assert(os.execute("7z"))
@@ -28,6 +64,14 @@ function check_tools()
   return true
 end
 
+-- TODO(geraldo) learn how to add more than one dir in 7z
+-- the alg will be something like this:
+--     is_on_size_limit
+--     if false: just zip everything, add timestamp and up
+--     if true: get oldest, remove it then check again is_on_size_limit
+-- TODO(geraldo) cleanup the config part, and figure out how
+-- will be the new dir structure.
+-- TODO(geraldo) to finalize, configure rclone to encrypt data
 function backup_dir(path_in, path_out)
   local file_name_out = path_out .. os.date("-%Y%m%d-%H%M%S") .. ".7z"
   local function_name = "7z a " .. file_name_out .. " " .. path_in
@@ -102,4 +146,6 @@ function main()
   end
 end
 
-main()
+-- main()
+
+print(return_oldest_file("googledrive:Backups/Finance"))
